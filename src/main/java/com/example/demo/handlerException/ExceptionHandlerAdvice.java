04114e0dafd29b4bbb0b5c245db57b6caa082f8f
wwowann <ivasin75@mail.ru>
2021-09-26 23:46:44 +0300

    ExceptionHandlerAdvice

--- src/main/java/com/example/demo/handlerException/ExceptionHandlerAdvice.java
+++ src/main/java/com/example/demo/handlerException/ExceptionHandlerAdvice.java
@@ -3,11 +3,13 @@ package com.example.demo.handlerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
+@ResponseStatus
 public class ExceptionHandlerAdvice {
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<String> invalidCredentials(NullPointerException e){
